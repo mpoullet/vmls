de9618963de6dfc989668093a0d8fea8e62d7535
Matthieu Poullet <matthieu.poullet@gmail.com>
2018-08-15 15:02:16 +0200

    Add a LS example.

--- src/vmls.cpp
+++ src/vmls.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
+
 #include <Eigen/Dense>
 
 using namespace Eigen;
 
 int main()
 {
-	MatrixXd m(2, 2);
-	m(0, 0) = 3;
-	m(1, 0) = 2.5;
-	m(0, 1) = -1;
-	m(1, 1) = m(1, 0) + m(0, 1);
-	std::cout << "Here is the matrix m:\n" << m << std::endl;
-	VectorXd v(2);
-	v(0) = 4;
-	v(1) = v(0) - 1;
-	std::cout << "Here is the vector v:\n" << v << std::endl;
-	return 0;
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+    std::cout << "Here is the matrix m:\n"
+              << m << std::endl;
+    VectorXd v(2);
+    v(0) = 4;
+    v(1) = v(0) - 1;
+    std::cout << "Here is the vector v:\n"
+              << v << std::endl;
+
+    MatrixXf A = MatrixXf::Random(3, 2);
+    VectorXf b = VectorXf::Random(3);
+    std::cout << "The least squares solution to Ax=b is\n"
+              << A.fullPivHouseholderQr().solve(b) << '\n';
+
+    return 0;
 }
